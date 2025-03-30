90250483df5d1c874f6350e70e34acd82322c27d
Luiz Henrique Pasin Laikovski <luizlaikovski@gmail.com>
2025-03-30 20:02:32 -0300

    ta funcionando

--- src/main/java/br/com/aplication/Main.java
+++ src/main/java/br/com/aplication/Main.java
@@ -14,36 +14,31 @@ import br.com.dto.TemperatureDto;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
-    // public final static String API_URL = "http://api.temperatureDtostack.com/current?access_key=" + API_KEY + "&query=";
-
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner sc = new Scanner(System.in);
-        Dotenv dotenv = Dotenv.configure().directory("C:\\Users\\luizl\\OneDrive\\Documentos\\GitHub\\professorOnline2-0\\professor-online2.0\\AppWeatherForecast\\AppWeatherForecast").load();
+        Dotenv dotenv = Dotenv.configure().load();
 
         String API_K = dotenv.get("API_KEY");
         String API_U = dotenv.get("API_URL");
-        System.out.println(API_U);
-        System.out.println(API_K);
 
         System.out.print("Digite a cidade que deseja consultar: "); String city = sc.next();
 
-        String URL_FINAL = API_U + city;
-        System.out.println(URL_FINAL);
+        String URL_FINAL = API_U + "?access_key=" + API_K + "&query=" + city;
         try {
             // 1. Faz a requisição HTTP
             HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(URL_FINAL))
-                .GET()
-                .build();
+                    .uri(URI.create(URL_FINAL))
+                    .GET()
+                    .build();
 
-            HttpResponse<String> response = HttpClient.newHttpClient()
-                .send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
 
             // 2. Verifica se a resposta foi bem-sucedida (código 200)
             if (response.statusCode() != 200) {
                 System.err.println("Erro na API: " + response.statusCode());
                 return;
             }
+
 
             // 3. Extrai apenas os campos necessários do JSON
             ObjectMapper mapper = new ObjectMapper();
